24613f43180f5428f48898ba5fb5ec590700909e
ahmedadelsayah <20240033@stud.fci-cu.edu.eg>
2026-08-23 15:39:55 +0300

    Add files via upload

---
+++ P1_Task Manger System/task_manager.py
@@ -0,0 +1,12 @@
+[
+    {
+        "id": 1,
+        "name": "Sales Review",
+        "status": "Completed"
+    },
+    {
+        "id": 2,
+        "name": "Profit Review",
+        "status": "Pending"
+    }
+]
